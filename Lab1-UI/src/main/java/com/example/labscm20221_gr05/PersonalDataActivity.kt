34ca27e15c75b613280e68ca2ef318c3b9ade3cd
Daniel Jaramillo <daniel.jaramillo7@udea.edu.co>
2022-06-20 17:44:23 -0500

    feat: add basic views to PersonalDataActivity

--- Lab1-UI/src/main/java/com/example/labscm20221_gr05/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/com/example/labscm20221_gr05/PersonalDataActivity.kt
@@ -2,10 +2,9 @@ package com.example.labscm20221_gr05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-class MainActivity : AppCompatActivity() {
+class PersonalDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_personal_data)
     }
 }
