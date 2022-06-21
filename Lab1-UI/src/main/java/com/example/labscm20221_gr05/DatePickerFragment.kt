6553eaf67303612e91cca38c6dcb5d2eecb0f42d
Daniel Jaramillo <daniel.jaramillo7@udea.edu.co>
2022-06-20 19:09:44 -0500

    feat: add basic datePicker dialog

---
+++ Lab1-UI/src/main/java/com/example/labscm20221_gr05/DatePickerFragment.kt
@@ -0,0 +1,19 @@
+package com.example.labscm20221_gr05
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+
+class DatePickerFragment(val listener: (year: Int, month: Int, day: Int) -> Unit): DialogFragment(), DatePickerDialog.OnDateSetListener  {
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        listener(year, month, dayOfMonth)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return DatePickerDialog(activity as Context, this, -1,-1,-1)
+    }
+
+}
--- Lab1-UI/src/main/java/com/example/labscm20221_gr05/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/com/example/labscm20221_gr05/PersonalDataActivity.kt
@@ -2,9 +2,24 @@ package com.example.labscm20221_gr05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
 class PersonalDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_data)
+        findViewById<Button>(R.id.button_date).setOnClickListener {
+            showDatePickerDialog()
+        }
+    }
+
+    private fun showDatePickerDialog() {
+        val datePicker = DatePickerFragment{year, month, day -> onDateSelected(year, month, day)}
+        datePicker.show(supportFragmentManager, "datePicker")
+    }
+
+    fun onDateSelected(year: Int, month: Int, day: Int) {
+        Log.d("Selected date", "${year.toString()} - ${month.toString()} - ${day.toString()}")
     }
 }
