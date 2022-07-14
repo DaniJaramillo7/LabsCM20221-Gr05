ed67155f0f33528a086250af3f1e273f393bd182
DESKTOP-SF5NH44\EQ01 <luis.rendon@bpt.com.co>
2022-07-13 20:36:01 -0500

    Se validan los datos con respecto a la información de contacto y se realiza su respectiva impresión

--- Lab1-UI/src/main/java/com/example/labscm20221_gr05/ContactData.kt
+++ Lab1-UI/src/main/java/com/example/labscm20221_gr05/ContactData.kt
@@ -2,59 +2,110 @@ package com.example.labscm20221_gr05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Button
 import android.widget.EditText
+import androidx.core.widget.doOnTextChanged
 import java.util.*
 
 class ContactData : AppCompatActivity() {
 
-    //private lateinit var citiesEditText: EditText;
+    private lateinit var phoneInput: EditText
+    private lateinit var addressInput: EditText
+    private lateinit var emailInput: EditText
+    private lateinit var countryInput: EditText
+    private lateinit var cityInput: EditText
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_data)
+
+        this.initializeInputs()
         this.listCountries()
-        this.setCities()
+        this.initializeCountryInput()
+        this.initializeButton()
+        this.initializeEmailInput()
+        this.initializePhoneInput()
+
 
     }
 
-    private fun listCountries(){
-        val conutryTexView = findViewById<AutoCompleteTextView>(R.id.input_country) as AutoCompleteTextView
+    private fun initializeInputs() {
+        this.phoneInput = findViewById<EditText>(R.id.input_phone)
+        this.addressInput = findViewById<EditText>(R.id.input_address)
+        this.emailInput = findViewById<EditText>(R.id.input_email)
+        this.countryInput = findViewById<AutoCompleteTextView>(R.id.input_country)
+        this.cityInput = findViewById<AutoCompleteTextView>(R.id.input_city)
+        this.button = findViewById<Button>(R.id.nextContact)
+    }
+
+    private fun onButtonContactNextTap() {
+        val phone = this.phoneInput.text.toString()
+        val address = this.addressInput.text.toString()
+        val email = this.emailInput.text.toString()
+        val country = this.countryInput.text.toString()
+        val city = this.cityInput.text.toString()
+        Log.d("Data", "Información de contacto")
+        Log.d("Data", "Telefono: $phone")
+        if (address != null) Log.d("Data", "Dirección $address")
+        Log.d("Data", "Email: $email")
+        Log.d("Data", "País: $country")
+        if (city != null) Log.d("Data", "Ciudad: $city")
+    }
+
+    private fun initializeButton() {
+        this.button.isEnabled = false
+        this.button.setOnClickListener() {
+            onButtonContactNextTap();
+        }
+
+    }
+
+    private fun listCountries() {
+        val conutryTexView =
+            findViewById<AutoCompleteTextView>(R.id.input_country) as AutoCompleteTextView
         val countries: Array<out String> = resources.getStringArray(R.array.countries)
-        ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,countries).also { arrayAdapter ->
+        ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1,
+            countries
+        ).also { arrayAdapter ->
             conutryTexView.setAdapter(arrayAdapter)
         }
     }
 
-    private fun setCities(){
-        var countriesEditText:EditText = findViewById(R.id.input_country)
-        countriesEditText.addTextChangedListener(object : TextWatcher{
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    private fun initializeCountryInput() {
+        this.countryInput
+            .doOnTextChanged { text, start, before, count ->
+                listCities(text.toString().uppercase(Locale.ROOT))
+                evaluateFormIsValid()
             }
+    }
 
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    private fun initializeEmailInput() {
+        this.emailInput
+            .doOnTextChanged { text, start, before, count ->
+                evaluateFormIsValid()
             }
+    }
 
-            override fun afterTextChanged(p0: Editable?) {
-
-                    listCities(countriesEditText.text.toString().uppercase(Locale.ROOT))
-
-
+    private fun initializePhoneInput() {
+        this.phoneInput
+            .doOnTextChanged { text, start, before, count ->
+                evaluateFormIsValid()
             }
-
-        })
     }
 
 
     private fun listCities(country: String) {
 
-            val cityTexView =
-                findViewById<AutoCompleteTextView>(R.id.input_city) as AutoCompleteTextView
-            val cities: Array<out String> = resources.getStringArray(R.array.cities)
-        if(country.equals("COLOMBIA")) {
+        val cityTexView =
+            findViewById<AutoCompleteTextView>(R.id.input_city) as AutoCompleteTextView
+        val cities: Array<out String> = resources.getStringArray(R.array.cities)
+        if (country.equals("COLOMBIA")) {
             ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
@@ -62,10 +113,18 @@ class ContactData : AppCompatActivity() {
             ).also { arrayAdapter ->
                 cityTexView.setAdapter(arrayAdapter)
             }
-        } else{
+        } else {
             cityTexView.setAdapter(null)
         }
 
+    }
+
+    private fun evaluateFormIsValid() {
+        val phoneIsValid = this.phoneInput.text.toString().trim().isNotEmpty()
+        val emailIsValid = this.emailInput.text.toString().trim().isNotEmpty()
+        val countrylIsValid = this.countryInput.text.toString().trim().isNotEmpty()
+        var formIsValid = phoneIsValid && emailIsValid && countrylIsValid
+        this.button.isEnabled = formIsValid
     }
 
 }
